953c14ec5d835ad15e01d5f87a8bcddc9e9a66b4
Daksh Raval <dakshraval717@gmail.com>
2025-10-13 17:59:07 -0600

    finally got grayscale images converted to cv format with cvbridge

--- src/aruco-corners-pub.py
+++ src/aruco-corners-pub.py
@@ -13,16 +13,25 @@ class ArucoCornersPub:
     def __init__(self): # constructor
         rospy.init_node('aruco_corners_pub')
 
-        # ~ means publish 'locally' under this node, eg. aruco_corners_pub/aruco_dict
-
         # zed mini subscribers
-        self.RGB_sub = rospy.Subscriber('/zedm/zed_node/left/image_rect_gray', Image, self.RGB_callback) 
-  
-        rospy.loginfo("Subscribed to ZED Mini RGB feed")
+        self.grayscale_sub = rospy.Subscriber('/zedm/zed_node/left/image_rect_gray', Image, self.grayscale_callback) 
+        rospy.loginfo("Subscribed to ZED Mini grayscale feed")
 
-    def RGB_callback(self, msg):
+        self.bridge = CvBridge() # instantiates CvBridge object, that converts RGB to format digestible by OpenCV
+
+    def grayscale_callback(self, msg):
         rospy.loginfo("Received an image message")
         rospy.loginfo(f"Image frame id: {msg.header.frame_id}, height: {msg.height}, width: {msg.width}")
+
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='mono8') # openCV likes bgr instead of rgb, and 8 means 
+            rospy.loginfo(f"Converted image shape: {cv_image.shape}, dtype: {cv_image.dtype}")
+
+            cv2.imshow("Image", cv_image)
+            cv2.waitKey(1)
+
+        except Exception as e:
+            rospy.logerr(f"Failed to convert image: {e}")
     
     def run(self):
         rospy.spin()
